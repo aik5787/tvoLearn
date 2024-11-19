1bca9e74d0daf442eea9f8add4448aad32fd1b9b
Aleksandr <aik5787@gmail.com>
2024-11-18 22:17:26 -0500

    cicd config

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://tvolearn.com/",
+    defaultCommandTimeout: 15000,
     viewportWidth: 1920,
     viewportHeight: 1080,
     reporter: "cypress-mochawesome-reporter",
